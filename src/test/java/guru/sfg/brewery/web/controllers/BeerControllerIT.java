916a2b3627acfe35476793d61971f2aeb989b5e9
TinaTiel <51816935+TinaTiel@users.noreply.github.com>
2020-12-05 10:04:00 +0100

    Commiting and pushing issue for course.

--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -11,10 +11,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -46,13 +48,15 @@ public class BeerControllerIT {
     void setUp() {
         mvc = MockMvcBuilders
                 .webAppContextSetup(wac)
+                .alwaysDo(print())
+                .apply(springSecurity())
                 .build();
     }
 
+    @WithMockUser("tina")
     @Test
     public void findBeers() throws Exception {
         mvc.perform(get("/beers/find"))
-                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("beers/findBeers"))
                 .andExpect(model().attributeExists("beer"));
